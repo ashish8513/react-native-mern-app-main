ed9a22de2c6cfcfe67357ec5573651f635ebfdfa
Ashish Prabhakar <141730191+ashish8513@users.noreply.github.com>
2024-10-22 00:46:04 +0530

    Update App.js

--- Frontend/App.js
+++ Frontend/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Ashish Prabhakar </Text>
+<Text>Please Follow and Subscribe me and Support me 🙏🙏 <Text/>
       <StatusBar style="auto" />
     </View>
   );
